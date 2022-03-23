6b135a2d0c95031174347cf3168f9f95e3e731b1
TheoChristo <theofilos_xri@yahoo.gr>
2022-03-23 15:33:42 +0200

    Running Domain Decomposition tests with MPI & ldconf

--- mpidocker/MSolveApp/Msolve.One.MPI/Msolve.One.MPI/Program.cs
+++ mpidocker/MSolveApp/Msolve.One.MPI/Msolve.One.MPI/Program.cs
@@ -7,10 +7,10 @@ namespace DotNetClient
     {
         static void Main(string[] args)
         {
-            // MpiTestSuite.RunTestsWith5Processes();
-            Hello(args);
+            // Hello(args);
             // HostNamesTest(args);
             // CommunicationTest(args);
+            MpiTestSuite.RunTestsWith5Processes();
 		}
 
         static void Hello(string[] args)
