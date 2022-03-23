d00bd76d3bc8b74bd0665cc61a9720e7bce3b9d5
TheoChristo <theofilos_xri@yahoo.gr>
2022-03-23 14:36:59 +0200

    Running Hello World

--- mpidocker/MSolveApp/Msolve.One.MPI/Msolve.One.MPI/Program.cs
+++ mpidocker/MSolveApp/Msolve.One.MPI/Msolve.One.MPI/Program.cs
@@ -1,4 +1,5 @@
-﻿using MGroup.Solvers.DDM.Tests;
+﻿using System;
+using MGroup.Solvers.DDM.Tests;
 
 namespace DotNetClient
 {
@@ -6,7 +7,75 @@ namespace DotNetClient
     {
         static void Main(string[] args)
         {
-            MpiTestSuite.RunTestsWith5Processes();
+            // MpiTestSuite.RunTestsWith5Processes();
+            Hello(args);
+            // HostNamesTest(args);
+            // CommunicationTest(args);
 		}
+
+        static void Hello(string[] args)
+        {
+            MPI.Environment.Run(ref args, communicator =>
+            {
+                Console.WriteLine("Hello, from process number "
+                                        + communicator.Rank + " of "
+                                        + communicator.Size);
+            });
+        }
+
+        static void CommunicationTest(string[] args)
+        {
+            MPI.Environment.Run(ref args, comm =>
+            {
+                if (comm.Size < 2)
+                {
+                    // Our ring needs at least two processes
+                    Console.WriteLine("The Ring example must be run with at least two processes.");
+                    Console.WriteLine("Try: mpiexec -np 4 ring.exe");
+                }
+                else if (comm.Rank == 0)
+                {
+                    // Rank 0 initiates communication around the ring
+                    string data = "Hello, World!";
+
+                    // Send "Hello, World!" to our right neighbor
+                    comm.Send(data, (comm.Rank + 1) % comm.Size, 0);
+
+                    // Receive data from our left neighbor
+                    comm.Receive((comm.Rank + comm.Size - 1) % comm.Size, 0, out data);
+
+                    // Add our own rank and write the results
+                    data += " 0";
+                    Console.WriteLine(data);
+                }
+                else
+                {
+                    // Receive data from our left neighbor
+                    String data;
+                    comm.Receive((comm.Rank + comm.Size - 1) % comm.Size, 0, out data);
+
+                    // Add our own rank to the data
+                    data = data + " " + comm.Rank.ToString() + ",";
+
+                    // Pass on the intermediate to our right neighbor
+                    comm.Send(data, (comm.Rank + 1) % comm.Size, 0);
+                }
+            });
+        }
+
+        static void HostNamesTest(string[] args)
+        {
+            MPI.Environment.Run(ref args, comm =>
+            {
+                string[] hostnames = comm.Gather(MPI.Environment.ProcessorName, 0);
+                if (comm.Rank == 0)
+                {
+                    Array.Sort(hostnames);
+                    foreach (string host in hostnames)
+                        Console.WriteLine(host);
+                }
+            });
+
+        }
     }
 }
