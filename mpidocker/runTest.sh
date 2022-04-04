1fbd7ccf4e6dd48b6fdb052b3c5653737a20c625
TheoChristo <theofilos_xri@yahoo.gr>
2022-04-04 15:22:04 +0300

    Testing migration to .Net6.0

--- mpidocker/runTest.sh
+++ mpidocker/runTest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This script assumes that it has been called from an MPI execution, e.g. `mpirun -np 2 runTest.sh` or `srun -n2 runTest.sh`
-dotnet /mpidocker/MSolveApp/Msolve.One.MPI/Msolve.One.MPI/bin/Release/net5.0/Msolve.One.MPI.dll
+dotnet /mpidocker/MSolveApp/Msolve.One.MPI/Msolve.One.MPI/bin/Release/net6.0/Msolve.One.MPI.dll
